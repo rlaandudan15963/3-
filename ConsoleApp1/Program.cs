7e51729c8b0d10e8b21ca5da9cb5778e996e1530
무영 김 <limm123@daum.net>
2022-09-26 21:22:22 +0900

    초기 커밋.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int a = 45;
+            int b = a - 5;
+            int c = b;
+            Console.WriteLine($" a = {a}, b = {b}, c = {c}");
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int a;
+            try
+            {
+                Console.Write("write number : ");
+                a = int.Parse(Console.ReadLine());
+                if (a > 0) { Console.WriteLine("this number is positive number"); }
+                else if (a == 0) { Console.WriteLine("this number is zero"); }
+                else if (a < 0) { Console.WriteLine("this number is negative number"); }
+            }
+            catch { Console.WriteLine("this is not a number"); }       
+        }
+    }
+}

18bab71ff863cef5da5561f69b3bf4d5e6af7350
무영 김 <limm123@daum.net>
2022-09-28 20:51:05 +0900

    3주차 연습 추가내용

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 54093422;
+            double b = 3.54;
+            Console.WriteLine("{0}, {1}", a, b);
+            Console.WriteLine("a = {0}, 0Xa = {0:X}", a);
+            Console.WriteLine("b = {0} = {0:E}", b);
+            Console.WriteLine("a = {0:X} = {0:E} = {0:N} = {0:C}", a);
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("++, -- program : ");
+            a = int.Parse(Console.ReadLine());
+            int b = a, c = a;
+            for(int i = 0; i < 5; i++)
+            {
+                ++b;
+                Console.WriteLine($"{b}");
+            }
+            Console.WriteLine("");
+            int l = 0;
+            while(l != 5)
+            {
+                --c;
+                Console.WriteLine($"{c}");
+                ++l;
+            }
+        }
+    }
+}
